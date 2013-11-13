de4f2a857e84a47f0627e8d594f5908051f5b2f2
Alexander Nikolaev <ainikolaev@gmail.com>
2013-11-13 23:04:20 +0400

    Better model!

--- EVM_Matlab/create_model.m
+++ EVM_Matlab/create_model.m
@@ -11,11 +11,11 @@
 %I also want to write out to a video file which could be viewed easily 
 %and into a matlab file that could be trivial to import.
 
-function create_model (x_size, y_size, base_color, alpha, length, ...
-                        framerate, noise_strength)
+function create_model (x_size, y_size, base_color, amp, length, ...
+                        framerate, noise_strength, phi, strength)
  
  resultsDir = 'ain_results-20131112model';
- filename='20131112_model6.avi'                   
+ filename='20131112_model7.avi'                   
  mkdir(resultsDir);
  
  vidOut = VideoWriter(fullfile(resultsDir,filename));
@@ -36,12 +36,12 @@ function create_model (x_size, y_size, base_color, alpha, length, ...
  end
  
  if isempty(base_color)
-     base_color(1,:)=[123 124 125];
+     base_color(1,:)=[123/255 124/255 125/255];
      %elseif check that base_color is an apprpriate array of doubles
  end
  
- if isempty(alpha)
-     alpha=20
+ if isempty(amp)
+     amp=[.02 .075 .015]
      %elseif check that it's a positive integer
  end
  
@@ -51,12 +51,22 @@ function create_model (x_size, y_size, base_color, alpha, length, ...
  end
  
  if isempty(noise_strength)
-     noise_setrength=1
+     noise_strength=1
      %elseif check that it's a positive double
  end
  
  if isempty(framerate)
      framerate=25
+     %elseif check that it's a positive integer
+ end
+ 
+  if isempty(phi)
+     phi=0
+     %elseif check that it's a positive integer
+  end
+ 
+   if isempty(strength)
+     strength=[1 1 1]
      %elseif check that it's a positive integer
  end
  
@@ -80,9 +90,9 @@ function create_model (x_size, y_size, base_color, alpha, length, ...
           %rand*base_color is too much fun
           %large alpha is too large
           %2*sin is too much
-          Stack(frame_num,x,y,1)=base_color(1)+alpha*.02*sin(frame_num/framerate)+0.1*(rand-.5);
-          Stack(frame_num,x,y,2)=base_color(2)+alpha*.075*sin(frame_num/framerate)+0.1*(rand-.5);
-          Stack(frame_num,x,y,3)=base_color(3)+alpha*.015*sin(frame_num/framerate)+0.1*(rand-.5);
+          Stack(frame_num,x,y,1)=cos(phi)*strength(1)*(base_color(1)+amp(1)*sin(frame_num/framerate))+0.1*(rand-.5);
+          Stack(frame_num,x,y,2)=cos(phi)*strength(2)*(base_color(2)+amp(2)*sin(frame_num/framerate))+0.1*(rand-.5);
+          Stack(frame_num,x,y,3)=cos(phi)*strength(3)*(base_color(3)+amp(3)*sin(frame_num/framerate))+0.1*(rand-.5);
       end
       
   end
