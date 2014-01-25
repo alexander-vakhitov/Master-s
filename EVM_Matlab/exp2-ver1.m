c4fb32019f18e476b590c23536ffdc375d46dba1
Alexander Nikolaev <ainikolaev@gmail.com>
2014-01-25 21:51:02 +0400

    New version of Optical Flow Stabilization

--- EVM_Matlab/exp2-ver1.m
+++ EVM_Matlab/exp2-ver1.m
@@ -1,6 +1,5 @@
 function exp2
-    %vidFile = 'F:\SPBSU\Masters\EVM_Matlab\data\warped.avi';
-    vidFile = 'c:\ResearchProjects\pulse_data\3780_cut1.mp4';
+    vidFile = 'F:\SPBSU\Masters\EVM_Matlab\data\MVI_3779.mov';
 
     % Read video
     vid = VideoReader(vidFile);
@@ -21,11 +20,7 @@ function exp2
     % firstFrame
     temp.cdata = read(vid, 1);
     [rgbframe, ~] = frame2im(temp);
-    rgbframe = im2double(rgbframe);
-    intervalx = [100:600];
-    intervaly = [350:850];
-    rgbframe = rgbframe(intervalx, intervaly, 1:3);
-    rgbframe = rgb2gray(rgbframe);
+    rgbframe = im2double(rgb2gray(rgbframe));
     
     % set optical flow parameters (see Coarse2FineTwoFrames.m for the definition of the parameters)
     alpha = 0.012;
@@ -36,8 +31,8 @@ function exp2
     nSORIterations = 30;
 
     para = [alpha,ratio,minWidth,nOuterFPIterations,nInnerFPIterations,nSORIterations];
-    
-    outName = 'warped_mar.avi';
+%where to write the output    
+    outName = '.\output\warped_MVI_3779.avi';
     
     vidOut = VideoWriter(outName);
     vidOut.FrameRate = fr;
@@ -45,21 +40,15 @@ function exp2
     open(vidOut);
 
     
-    
-    for i = 1:len
+     for i = 100:125 
+  %  for i = 100:len
+        %cut out the first few seconds
         temp.cdata = read(vid, i);
         [rgbframe2, ~] = frame2im(temp);
-        rgbframe2 = im2double(rgbframe2);
-        rgbframe2 = rgbframe2(intervalx, intervaly, 1:3);
-        rgbframe2gr = rgb2gray(rgbframe2);
-        [vx,vy,warpI2] = Coarse2FineTwoFrames(rgbframe,rgbframe2gr,para);
-        sz = size(rgbframe);
-        [y x] = ndgrid( 1:sz(1), 1:sz(2) );
-        timg2(:, :, 1) = interp2( x, y, rgbframe2(:, :, 1), x + vx, y + vy );
-        timg2(:, :, 2) = interp2( x, y, rgbframe2(:, :, 2), x + vx, y + vy );
-        timg2(:, :, 3) = interp2( x, y, rgbframe2(:, :, 3), x + vx, y + vy );
+        rgbframe2 = im2double(rgb2gray(rgbframe2));
+        [vx,vy,warpI2] = Coarse2FineTwoFrames(rgbframe,rgbframe2,para);
         norm(vx)
-        writeVideo(vidOut,im2uint8(timg2));
+        writeVideo(vidOut,im2uint8(warpI2));
     end
     
     close(vidOut);
